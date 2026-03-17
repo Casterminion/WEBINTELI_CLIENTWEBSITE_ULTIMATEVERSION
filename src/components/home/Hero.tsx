7e4c0c71e60e897d107f49d2d80a94ccfd43d9c0
Casterminion <arijusblaviesciunas@gmail.com>
2026-03-17 13:27:11 +0200

    dsds

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -83,7 +83,7 @@ const Hero: React.FC = () => {
                   aria-label="Hero video preview"
                 >
                   <source src="/wp-content/uploads/2024/11/background.mp4" type="video/mp4" />
-                  <track kind="captions" src="/captions-empty.vtt" srclang="lt" label="Lietuvių" />
+                  <track kind="captions" src="/captions-empty.vtt" srcLang="lt" label="Lietuvių" />
                 </video>
               </div>
             </div>
@@ -126,7 +126,7 @@ const Hero: React.FC = () => {
                 aria-label="Hero video preview"
               >
                 <source src="/wp-content/uploads/2024/11/background.mp4" type="video/mp4" />
-                <track kind="captions" src="/captions-empty.vtt" srclang="lt" label="Lietuvių" />
+                <track kind="captions" src="/captions-empty.vtt" srcLang="lt" label="Lietuvių" />
               </video>
               <button
                 type="button"
